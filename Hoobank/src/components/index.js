8222ae7893c4fcdb9c0c78c109a5db2bb980033c
Emmanuel Ogbuji <emmanuelogbuji5@gmail.com>
2023-06-20 00:26:02 +0100

    First commit

---
+++ Hoobank/src/components/index.js
@@ -0,0 +1,24 @@
+import Navbar from "./Navbar";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Billing from "./Billing";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+import Footer from "./Footer";
+
+export {
+Navbar,
+Business,
+CardDeal,
+Clients,
+CTA,
+Billing,
+Hero,
+Stats,
+Testimonials,
+Footer,
+    
+};
