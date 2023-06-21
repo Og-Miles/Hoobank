1e9a94e3b2f6599dba494348450ed0311e8a2669
Emmanuel Ogbuji <emmanuelogbuji5@gmail.com>
2023-06-21 14:07:59 +0100

    Billing section

--- Hoobank/src/components/Business.jsx
+++ Hoobank/src/components/Business.jsx
@@ -25,7 +25,7 @@ const Business = () => {
     <section id="features" className={layout.section}>
         <div className={layout.sectionInfo}>
           <h2 className={styles.heading2}>You do the business, <br className="sm:block hidden"/> we'll handle the money.</h2>
-          <p className={`${styles.paragraph} max-w-[470px] mt-5`}>With the right card, you can improve your financial life by building credit, earining rewards and saving moey. BUt with hundreds of credit cards on the market.</p>
+          <p className={`${styles.paragraph} max-w-[470px] mt-5`}>With the right card, you can improve your financial life by building credit, earining rewards and saving moey. But with hundreds of credit cards on the market.</p>
           <Button styles='mt-10'/>
           
         </div>
