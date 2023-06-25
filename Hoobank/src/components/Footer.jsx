5417d7266c51458dd6f673d4646ea40c0ba2bb48
Miles <emmanuelogbuji5@gmail.com>
2023-06-25 23:49:47 +0100

    Footer

--- Hoobank/src/components/Footer.jsx
+++ Hoobank/src/components/Footer.jsx
@@ -6,6 +6,7 @@ const Footer = () => (
     <section className={`${styles.flexCenter} ${styles
     .paddingY} flex-col`}>
       <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+        {/* First colum (Logo and email) */}
         <div className="flex-1 flex flex-col justify-start mr-10">
           <img src={logo} alt="Hoobank" className="w-[266px] h-[72px] object-contain"/>
           <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>A new way to make the payments easy reliable and secure</p>
