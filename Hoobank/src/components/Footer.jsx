390ba94a9e1843ef6ac7abb5fe9eba44f34182e0
Miles <emmanuelogbuji5@gmail.com>
2023-06-26 06:33:11 +0100

    Last commit

--- Hoobank/src/components/Footer.jsx
+++ Hoobank/src/components/Footer.jsx
@@ -33,7 +33,8 @@ const Footer = () => (
         </div>
 
       </div>
-              // Copyright and socials //
+              {/* Copyright and socials*/}
+
           <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3e45]">
             <p className="font-poppins font-normal text-center text-18px leading-7px text-white">2023 Hoobank. All Rights Reserved</p> 
             <div className="flex flex-row md:mt-0 mt-6">
