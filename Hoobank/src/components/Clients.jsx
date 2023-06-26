28152b06a69d5673a46320c793a22a40f93ef3f0
Miles <emmanuelogbuji5@gmail.com>
2023-06-26 07:48:45 +0100

    Clients

--- Hoobank/src/components/Clients.jsx
+++ Hoobank/src/components/Clients.jsx
@@ -4,7 +4,7 @@ import Button from "./Button";
 
 const Clients = () => (
     <section className={`${styles.flexCenter} my-4`}>
-      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      <div className={`${styles.flexCenter} flex flex-wrap w-full`}>
         {clients.map((client) => (
             <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-2-[120px]`}>
                 <img src={client.logo} alt="clients" className="sm:w-[192px] w-[100px] object-contain" />  
